8ad213c9da5a834a6594fe81626045cecb9896b2
John Price <136085120+JackErRip@users.noreply.github.com>
2023-07-22 13:08:08 +0800

    Dawg_Squad

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="headwrap">
+     
+      <header className="App-header">
+        <center>
+        
+          <h1>Homepage</h1>
+          
+       <br />
+        </center>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
