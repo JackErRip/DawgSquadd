d7074f4c00581230aa18b5772288623b5e73136b
John Price <136085120+JackErRip@users.noreply.github.com>
2023-07-22 20:03:55 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import './App.css';
 import React from 'react';
+import './App.css';
+
+
 
 const WebsiteOpener = () => {
   const openWebsite = () => {
@@ -7,32 +9,50 @@ const WebsiteOpener = () => {
     window.location.href = url;
   };
 
+  const onlineImageUrl = 'https://scontent.fcgy2-2.fna.fbcdn.net/v/t39.30808-6/336098390_1290576248205706_6307620226466032412_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=Qtlq3wbzk34AX96vssC&_nc_ht=scontent.fcgy2-2.fna&_nc_e2o=c&oh=00_AfCZIO1CYsYHqHpdlEH4v8WYPIt6xAaSLTjSeTyHOH0mHw&oe=64C0FC90'; // Replace with the URL of the online image
 
   return (
-    <html>
-      <body>
     <div className="App">
       <header className="App-header">
-      <title>The Dawg Squad Website</title>
+        <title>The Dawg Squad Website</title>
+        
 
+        <div className="content-container">
 
-    
-      <button className="largeButton" onClick={openWebsite}>Ghost Squad</button>
-      
-        <h1>Homepage</h1>
+        
+          <img
+            src={onlineImageUrl}
+            alt="Logo of the Dawg Squad website"
+            width="192" // Set the desired width for the small image
+            height="108" // Set the desired height for the small image>
+
+          />
+</div>
+          <div className="text-container">
+            
+            <h1>Homepage</h1>
+            </div>
+   
+          <div className="large-button">
+          <button className="largeButton" onClick={openWebsite}>
+              Ghost Squad
+            </button>
+           
+
+            </div>
+
+        
+
+          <div className="stupid-string-easter">
+          <p>Website was made from scratch using and made by this guy ---> </p>
+
+          </div>
+
+          
 
       </header>
-
-     
-
+       
     </div>
-    </body>
-  </html>
   );
-}
-
-
-
-
-
+};
 export default WebsiteOpener;
